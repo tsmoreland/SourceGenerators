e7fe533289b9b3f8b50666776d66da73cf2e69d3
Terry Moreland <tsmoreland@live.com>
2021-11-16 22:18:44 +0000

    sample class using generators

---
+++ TSMoreland.ExceptionSourceGenerator.Consumer/SampleException.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics;
+using TSMoreland.ExceptionSourceGenerator.Shared;
+
+namespace TSMoreland.ExceptionSourceGenerator.Consumer;
+
+[DebuggerDisplay("Sample Exception {Message}")]
+[AddExceptionConstructors]
+public class SampleException
+{
+}
