54fea8b0a06647a0fdd3f5be37282ebdb092477c
Terry Moreland <tsmoreland@live.com>
2021-11-16 22:18:30 +0000

    shared class providing attribute(s) to identify which classes should have generation applied

---
+++ TSMoreland.ExceptionSourceGenerator.Shared/AddExceptionConstructorsAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace TSMoreland.ExceptionSourceGenerator.Shared;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+public sealed class AddExceptionConstructorsAttribute : Attribute
+{
+
+    /// <summary>
+    /// Constructs a new instance of the <see cref="AddExceptionConstructorsAttribute"/> class.
+    /// </summary>
+    public AddExceptionConstructorsAttribute()
+    {
+    }
+
+}
