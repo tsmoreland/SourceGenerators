ae5ae54a7c8bf8afec6a7c342030e30165f7e4bb
Terry Moreland <tsmoreland@live.com>
2021-11-18 19:44:04 +0000

    renamed attribute and changed to allow it to be added multiple times it now stores details of a single property type, name, default value and whether it's readonly

---
+++ TSMoreland.ExceptionSourceGenerator.Shared/ExceptionGeneratorAttribute.cs
@@ -0,0 +1,39 @@
+﻿namespace TSMoreland.ExceptionSourceGenerator.Shared;
+
+/// <summary>
+/// Marks a class for application of source generator, if present the constructors
+/// and properties are included.
+/// </summary>
+/// <remarks>
+/// can be used multiple times to handle multiple properties
+/// </remarks>
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
+public sealed class ExceptionGeneratorAttribute : Attribute
+{
+
+    /// <summary>
+    /// Constructs a new instance of the <see cref="ExceptionGeneratorAttribute"/> class.
+    /// </summary>
+    public ExceptionGeneratorAttribute()
+    {
+    }
+    /// <value>
+    /// Type for a property to be included in the exception
+    /// </value>
+    public string? PropertyType { get; set; }
+
+
+    /// <value>
+    /// Name for a property to be included in the exception
+    /// </value>
+    public string? PropertyName { get; set; }
+
+    /// <value>
+    /// </value>    
+    public string? PropertyDefaultValue { get; set; }
+
+    /// <value>
+    /// if <see langword="true"/> property be readonly
+    /// </value>
+    public bool IsReadOnly { get; set; }
+}
