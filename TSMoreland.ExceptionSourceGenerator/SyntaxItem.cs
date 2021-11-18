3adacb700bcde364ed15caa27db527cf75f9ad6d
Terry Moreland <tsmoreland@live.com>
2021-11-18 22:29:25 +0000

    attempting ISyntaxReceiver use, though will likely abandon it moved SyntaxItem to its own file

---
+++ TSMoreland.ExceptionSourceGenerator/SyntaxItem.cs
@@ -0,0 +1,9 @@
+﻿namespace TSMoreland.ExceptionSourceGenerator;
+
+internal sealed class SyntaxItem
+{
+    public string Namespace { get; init; } = string.Empty;
+    public string ClassName { get; init; } = string.Empty;
+
+    public string Fullname => $"{Namespace}.{ClassName}";
+}
