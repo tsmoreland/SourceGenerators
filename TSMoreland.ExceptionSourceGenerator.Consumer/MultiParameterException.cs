97538f38fd98ec9bf11deffb253e802142a0df16
Terry Moreland <tsmoreland@live.com>
2021-11-22 02:22:23 +0000

    additional exception types for further testing

---
+++ TSMoreland.ExceptionSourceGenerator.Consumer/MultiParameterException.cs
@@ -0,0 +1,9 @@
+﻿using TSMoreland.ExceptionSourceGenerator.Shared;
+
+namespace TSMoreland.ExceptionSourceGenerator.Consumer;
+
+[ExceptionGenerator(IsReadOnly = true, PropertyName = "Alpha", PropertyType = "int")]
+[ExceptionGenerator(IsReadOnly = true, PropertyName = "Bravo", PropertyType = "System.Guid")]
+public partial class MultiParameterException
+{
+}
