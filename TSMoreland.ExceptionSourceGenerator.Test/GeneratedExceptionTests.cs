8669e06e029f318e51fea4b77a56059c051de817
Terry Moreland <tsmoreland@live.com>
2021-12-10 00:31:40 +0000

    re-enable and reformat tests

--- TSMoreland.ExceptionSourceGenerator.Test/GeneratedExceptionTests.cs
+++ TSMoreland.ExceptionSourceGenerator.Test/GeneratedExceptionTests.cs
@@ -12,26 +12,26 @@ public class GeneratedExceptionTests
     [Test]
     public void SimpleException_ReturnsMessage_WhenGeneratedAndGivenMessage()
     {
-        const string expected = "51FA53CE-96B7-43E5-8B6F-BA5BEBD1C23F";
+        const string expected = "sample message";
         var exception = new SimpleException(expected);
-        var actual = exception.Message;
+        string actual = exception.Message;
         Assert.That(actual, Is.EqualTo(expected));
     }
 
     [Test]
     public void SimpleException_ReturnsMessage_WhenGeneratedAndGivenException()
     {
-        const string expected = "CD054D6C-07B4-4A8E-8D9B-4FBC77B1CA13";
+        const string expected = "sample message";
         var inner = new NotImplementedException("not implemented");
         var exception = new SimpleException(expected, inner);
-        var actual = exception.Message;
+        string actual = exception.Message;
         Assert.That(actual, Is.EqualTo(expected));
     }
 
     [Test]
     public void SimpleException_ReturnsInnerException_WhenGeneratedAndGivenException()
     {
-        var expected = new NotImplementedException("not implemented");
+        NotImplementedException expected = new("not implemented");
         var exception = new SimpleException("message", expected);
         var actual = exception.InnerException;
         Assert.That(actual, Is.EqualTo(expected));
